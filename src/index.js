53f6505106389d8dad96a6712a8467cd57ec99d3
istrelkina <istrelkina@gmail.com>
2022-02-25 22:55:07 +1000

    feat: add part of desigion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isFlag = false;
+  let lastElement;
+  let arrStack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    isFlag = false;
+    // console.log('str[i] = ',str[i]);
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        // console.log('push');
+        arrStack.push(str[i]);
+        isFlag = true;
+      };
+    };
+    if (isFlag == false) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        //  console.log('j=',j);
+        if (str[i] == bracketsConfig[j][1]) {
+
+          if (arrStack.length != 0) {
+            //  console.log('Совпадение ', bracketsConfig[j][1]);
+            lastElement = arrStack[arrStack.length - 1];
+            if (lastElement == bracketsConfig[j][0]) {
+              arrStack.pop();
+              // console.log('pop', arrStack);
+            } else return false;
+          } else return false;
+        }
+      }
+    };
+
+  }
+
+  if (arrStack.length === 0) { return true } else { return false };
 }
